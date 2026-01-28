f19b9bc24751b7ffa8f146a60d00dc1b3c1cde9f
Josh Davis <josh.davis@live.ca>
2026-01-28 01:27:43 -0500

    refactored based on chatGPT advice

--- script.js
+++ script.js
@@ -12,74 +12,63 @@ async function replaceTiles(tiles) {
   })
 }
 
+// calculates the sweepFlag of an SVG path arc "A r,r 0 0 sweepFlag, arcEnd"
+// right turn -> 1
+// left turn -> 0
+function sweepFlag(dIn, dOut) {
+  const cross = dIn.x * dOut.y - dIn.y * dOut.x;
+  return cross < 0 ? 0 : 1;
+}
+
 function setClipPath(tile) {
   const d="M 0.025,0.1 L 0.4875,0.1 A0.025,0.025 0,0,0 0.5125,0.0875 L 0.5125,0.0125 A0.025,0.025 0,0,1 0.5375,0 L 0.575,0 L 0.975,0 A0.025,0.025 0,0,1 1,0.025 L 1,0.975 A0.025,0.025 0,0,1 0.975,1 L 0.025,1 A0.025,0.025 0,0,1 0,0.975 L 0,0.125 A0.025,0.025 0,0,1 0.025,0.1 Z"
   
-  const cornerRadius = 0.1;
+  const cornerRadius = 0.05;
   const cutoutDepth = 0.1;
   const cutoutWidth = 0.5;
-  const coords = [[0+cornerRadius, cutoutDepth, null, null], [cutoutWidth, cutoutDepth, 'up', 'A0'], [cutoutWidth, 0, 'right', 'A1'], [1,0,'down','A1'], [1,1,'left','A1'], [0,1,'upLeft','A1'], [0,cutoutDepth,'right','A1'], [0+cornerRadius, cutoutDepth, null,null]];
-  // const arcs = ['L', 'A0', 'L', 'A1', 'L', 'A1', 'L', 'A1', 'L', 'A1', 'L', 'A1'];
+
+  const coords = [
+    {x: cornerRadius, y: cutoutDepth},
+    {x: cutoutWidth, y: cutoutDepth, in: 'right', out: 'up'},
+    {x: cutoutWidth, y: 0, in: 'up', out: 'right'},
+    {x: 1, y: 0, in: 'right', out: 'down'},
+    {x: 1, y: 1, in: 'down', out: 'left'},
+    {x: 0, y: 1, in: 'left', out: 'up'},
+    {x: 0, y: cutoutDepth, in: 'up', out: 'right'},
+    {x: cornerRadius, y: cutoutDepth},
+  ];
+
   const dirs = {
-    up: {
-      pt1: {x: -1, y: 0},
-      pt2: {x: 0, y: -1}
-    },
-    right: {
-      pt1: {x: 0, y: 1},
-      pt2: {x: 1, y: 0}
-    },
-    down: {
-      pt1: {x: -1, y: 0},
-      pt2: {x: 0, y: 1}
-    },
-    left: {
-      pt1: {x: 0, y: -1},
-      pt2: {x: -1, y: 0}
-    },
-    upLeft: {
-      pt1: {x: 1, y: 0},
-      pt2: {x: 0, y: -1}
-    }, 
+    up: {x: 0, y: -1},
+    right: {x: 1, y: 0},
+    down: {x: 0, y: 1},
+    left: {x: -1, y: 0},
   };
-
-  arcLookup = {
-    L: 'L',
-    A0: `A${cornerRadius},${cornerRadius} 0,0,0`,
-    A1: `A${cornerRadius},${cornerRadius} 0,0,1`,
-  }
   
-  let pathd = 'M';
-  let arcCounter = 0;
-  for (const [index,[x,y,dir,arc]] of coords.entries()) {
-    if (arc == null) {
-      str = ` ${x},${y} L`;
-      pathd += str;
-      console.log(str)
-    }
-    
-    // if (index == 0) {
-    //   pathd += ` ${x},${y} ${arcLookup[arcs[arcCounter]]}`;
-    //   arcCounter++;
-    // }
-    // else if (index == coords.length - 1) {
-    //   pathd += ` ${arcLookup[arcs[arcCounter]]} ${x},${y} Z`
-    // }
-    else {
-      const pt1 = `${x + cornerRadius*dirs[dir].pt1.x},${y + cornerRadius/2*dirs[dir].pt1.y}`;
-      const pt2 = `${x + cornerRadius*dirs[dir].pt2.x},${y + cornerRadius/2*dirs[dir].pt2.y}`;
-      // const arc1 = arcLookup[arcs[arcCounter]];
-      // arcCounter++;
-      // const arc2 = arcLookup[arcs[arcCounter]];
-      // arcCounter++;
-      str = ` ${pt1} L ${arcLookup[arc]} ${pt2} L`;
-      pathd += str;
-      console.log(str)
+  let pathd = `M ${coords[0].x},${coords[0].y}`;
+  for (let i = 1; i < coords.length - 1; i++) {
+    const curr = coords[i];
 
-    }
-    // console.log(pathd)
+    const dIn = dirs[curr.in];
+    const dOut = dirs[curr.out];
+
+    // arc geometry
+    const arcStart = {
+      x: curr.x - cornerRadius * dIn.x,
+      y: curr.y - cornerRadius * dIn.y,
+    };
+
+    const arcEnd = {
+      x: curr.x + cornerRadius * dOut.x,
+      y: curr.y + cornerRadius * dOut.y,
+    };
+
+    const sweep = sweepFlag(dIn, dOut);
+
+    pathd += ` L ${arcStart.x},${arcStart.y} A ${cornerRadius},${cornerRadius} 0 0 ${sweep} ${arcEnd.x},${arcEnd.y}`;
   }
-  pathd = pathd.slice(0,-1) + 'Z'
+  const lastCoord = coords.at(-1);
+  pathd += ` L ${lastCoord.x},${lastCoord.y} Z`;
   console.log(pathd)
 
   const clipPath = tile.querySelector('.clip-path')
